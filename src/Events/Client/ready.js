ce108302635762d2ddda42470516995f8da587c9
te94d <te94da@gmail.com>
2022-11-15 00:19:35 +0900

    mongoDB archives

--- src/Events/Client/ready.js
+++ src/Events/Client/ready.js
@@ -1,18 +1,10 @@
 const {Client} = require('discord.js');
-const mongoose = require('mongoose');
 const config = require("../../../config.json")
 
 module.exports = {
   name: "ready",
   once: true,
   async execute(client) {
-    await mongoose.connect(config.mongodb || '', {
-      keepAlive: true,
-    });
-
-    if(mongoose.connect) {
-      console.log('MongoDB connection succesful.')
-    }
 
     console.log(`${client.user.username} is now online.`);
   },
